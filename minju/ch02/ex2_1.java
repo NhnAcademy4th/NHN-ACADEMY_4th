17bac1c974bf426bdf83ec869ddfb4548f101a0f
김민주 <ks00919@naver.com>
2023-09-12 13:05:31 +0900

    파일 업로드

---
+++ minju/ch02/ex2_1.java
@@ -0,0 +1,13 @@
+public class ex2_1 {
+    public static void main(String[] args) {
+        System.out.println("******           *************        **********");
+        System.out.println("**    **                **            **");
+        System.out.println("**     **               **            **");
+        System.out.println("**      **              **            **");
+        System.out.println("**      **              **            ********");
+        System.out.println("**      **       **     **            **");
+        System.out.println("**     **         **    **            **");
+        System.out.println("**    **           **  **             **");
+        System.out.println("*****               ****              ********** ");
+    }
+}
---
+++ minju/ch02/ex2_5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex2_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("how many eggs : ");
+        int eggs = scanner.nextInt();
+
+        int gross = eggs / 144;
+        eggs = eggs % 144;
+
+        int dozen = eggs / 12;
+        eggs = eggs % 14;
+
+        System.out.println("Your number of eggs is " + gross + " gross, " + dozen + " dozen, and " + eggs);
+    }
+}
