98073fe6628612330db93596d82b622d952bbb18
sonnysson <cloudy4047@gmail.com>
2023-09-12 11:07:36 +0900

    멤버 파일 생성

---
+++ 손병민/ch02/ex2_6.java
@@ -0,0 +1,20 @@
+package com.example.ch02;
+
+import java.util.Scanner;
+
+public class ex2_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter your first name and last name, separated by a space.");
+        System.out.print("?");
+
+        String name = sc.nextLine();
+        String firstName = name.substring(0, name.indexOf(" "));
+        String lastName = name.substring(name.indexOf(" ") + 1);
+
+        System.out.println("Your first name is " + firstName + ", which has " + firstName.length() + " characters.");
+        System.out.println("Your last name is " + lastName + ", which has " + lastName.length() + " characters.");
+
+    }
+}
---
+++ 손병민/ch02/ex2_7.java
@@ -0,0 +1,11 @@
+package com.example.ch02;
+
+import java.io.FileReader;
+
+public class ex2_7 {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader reader = new FileReader(
+                "/Users/sonnyson/Desktop/javaFX_test/src/main/java/com/example/ch02/testdata.txt");
+        // src/main/java/com/example/ch02/testdata.txt
+    }
+}
