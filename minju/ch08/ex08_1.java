14be8aa37362310f536f6cb0b31ecbae1b42ec46
김민주 <69795199+ks00919@users.noreply.github.com>
2023-09-22 15:54:35 +0900

    upload / 8장 1, 2번

---
+++ minju/ch08/ex08_1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+/**
+ * ex08_1
+ */
+public class ex08_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+
+                System.out.print("Enter A : ");
+                String inputA = scanner.nextLine().toLowerCase();
+
+                System.out.print("Enter B : ");
+                String inputB = scanner.nextLine().toLowerCase();
+
+                System.out.print("Enter C : ");
+                String inputC = scanner.nextLine().toLowerCase();
+
+                if (inputA.equals("no") || inputB.equals("no") || inputC.equals("no")) {
+                    System.out.println("종료합니다.");
+                    break;
+                }
+
+                String check = "\\d+(.\\d)?";
+                if (!(inputA.matches(check) && inputB.matches(check) && inputC.matches(check))) {
+                    System.out.println("값이 잘못 입력되었습니다. 다시 입력해주세요.");
+                    continue;
+                }
+
+                double A = Double.parseDouble(inputA);
+                double B = Double.parseDouble(inputB);
+                double C = Double.parseDouble(inputC);
+                System.out.println("result : " + root(A, B, C));
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scanner.close();
+    }
+
+    static public double root(double A, double B, double C)
+            throws IllegalArgumentException {
+        if (A == 0) {
+            throw new IllegalArgumentException("A can't be zero.");
+        } else {
+            double disc = B * B - 4 * A * C;
+            if (disc < 0)
+                throw new IllegalArgumentException("Discriminant < zero.");
+            return (-B + Math.sqrt(disc)) / (2 * A);
+        }
+    }
+}
---
+++ minju/ch08/ex08_2.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ex08_2 {
+
+    public static void main(String[] args) {
+        System.out.print("숫자를 입력해주세요 : ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            BigInteger number = new BigInteger(scanner.nextLine());
+            collatz(number);
+        } catch (NumberFormatException e) {
+            System.out.println("값이 잘못 입력되었습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    static public BigInteger collatz(BigInteger n) {
+        if (n.signum() == -1) {
+            throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
+        }
+
+        System.out.print(n + " ");
+
+        if (n.signum() == 0 || n.equals(new BigInteger("1"))) {
+            return n;
+        }
+
+        if (!n.testBit(0)) {
+            return collatz(n.divide(new BigInteger("2")));
+        }
+
+        return collatz(n.multiply(new BigInteger("3")).add(new BigInteger("1")));
+    }
+}
