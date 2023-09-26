9cbdee477ae939f4115ab6d3e08e3e769d3cb5a8
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-09-26 10:54:08 +0900

    재훈 - refactor : 과제 변수명 수정

--- jaehun/assignment/movie search/src/Movie.java
+++ jaehun/assignment/movie search/src/Movie.java
@@ -4,18 +4,18 @@ import java.util.Map;
 import java.util.Set;
 
 public class Movie {
-    private static final String[] infoOfKind =
+    private static final String[] kindOfInfo =
             {"MovieID", "Title", "KoreanTitle", "Plot", "ReleaseYear", "RunningTime", "GradeID", "GradeInKoreaID",
                     "Poster", "ReleaseDateInKorea", "BoxOfficeWWGross", "BoxOfficeUSGross", "Budget", "OriginalAuthor",
                     "OriginalSource"};
     private Map<String, String> movieInfo = new LinkedHashMap<>();
     public Movie(List<String> movieInfoList) {
-        if (movieInfoList.size() != infoOfKind.length) {
+        if (movieInfoList.size() != kindOfInfo.length) {
             throw new IllegalArgumentException("파싱이 제대로 이루어지지 않았습니다.");
         }
 
-        for (int i = 0; i < infoOfKind.length; i++) {
-            this.movieInfo.put(infoOfKind[i], movieInfoList.get(i));
+        for (int i = 0; i < kindOfInfo.length; i++) {
+            this.movieInfo.put(kindOfInfo[i], movieInfoList.get(i));
         }
     }
 
