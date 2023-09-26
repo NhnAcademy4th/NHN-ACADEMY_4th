266619aead06f123f4e46117a634096bc723cdd7
김민주 <69795199+ks00919@users.noreply.github.com>
2023-09-26 17:57:21 +0900

    upload: 폴더 이동

--- minju/과제/movie/MovieSearchShell.java
+++ minju/과제/movie/MovieSearchShell.java
@@ -9,7 +9,6 @@ public class MovieSearchShell {
     public static void startMovieShell() {
         try (Scanner scanner = new Scanner(System.in)) {
             MovieList list = MovieParser.load(System.getProperty("user.dir") + "/Movie.csv");
-
             while (true) {
                 System.out.print("검색할 영화를 입력해주세요(enter시 종료) : ");
                 String answer = scanner.nextLine();
