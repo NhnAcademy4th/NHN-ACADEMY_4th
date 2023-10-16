28653d9fe752cd353d33c90bc16aba96d44171d8
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-16 17:47:02 +0900

    upload: 10장 2,3,4번

---
+++ minju/ch10/ex10_4.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ex10_4 {
+    public static <T> void remove(Collection<T> coll, Predicate<T> pred) {
+        for (T item : coll) {
+            if (pred.test(item)) {
+                coll.remove(item);
+            }
+        }
+    }
+
+    public static <T> void retain(Collection<T> coll, Predicate<T> pred) {
+        for (T item : coll) {
+            if (!pred.test(item)) {
+                coll.remove(item);
+            }
+        }
+    }
+
+    public static <T> List<T> collect(Collection<T> coll, Predicate<T> pred) {
+        List<T> list = new ArrayList<>();
+        for (T item : coll) {
+            if (pred.test(item))
+                list.add(item);
+        }
+        return list;
+    }
+
+    public static <T> int find(ArrayList<T> list, Predicate<T> pred) {
+        for (int i = 0; i < list.size(); i++) {
+            if (pred.test(list.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
