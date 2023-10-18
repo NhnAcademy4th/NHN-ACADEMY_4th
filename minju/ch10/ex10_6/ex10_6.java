4a051cfcd1728145324e484d95133310c382d0d2
김민주 <69795199+ks00919@users.noreply.github.com>
2023-10-18 09:31:28 +0900

    upload: 10장 6번

---
+++ minju/ch10/ex10_6/ex10_6.java
@@ -0,0 +1,12 @@
+package ex10_6;
+
+public class ex10_6 {
+    public static void main(String[] args) {
+        try {
+            new ConcordanceReader(System.getProperty("user.dir") + "/ex10_6/test.txt")
+                    .writeTextFile(System.getProperty("user.dir") + "/ex10_6/result.txt");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
